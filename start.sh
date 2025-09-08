70bf90f2ef6ec3aa7ad4fafeaa1aefd46fdfa3d6
moffatmubea <48717239+mubeakungu@users.noreply.github.com>
2025-09-08 09:34:49 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env sh
-
-Install dependencies from requirements.txt
 pip install -r requirements.txt
-
-Start the Gunicorn server. The --bind flag tells Gunicorn to listen on
-all public IPs, using the port provided by Render's environment variable.
 gunicorn --bind 0.0.0.0:$PORT app:app
