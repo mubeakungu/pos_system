766427854e2a387bea7b95c0e6f4d681fd82e191
moffatmubea <48717239+mubeakungu@users.noreply.github.com>
2025-09-08 15:05:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 import datetime
