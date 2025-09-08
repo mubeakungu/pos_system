c87595ee74cccb006e55df637fe9be86d77cfb3e
moffatmubea <48717239+mubeakungu@users.noreply.github.com>
2025-09-08 09:28:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-# Use Gunicorn to run the Flask application in production
-# The --bind flag tells Gunicorn to listen on all public IPs, using the port
-# provided by Render's environment variable.
+Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+Start the Gunicorn server
+The --bind flag tells Gunicorn to listen on all public IPs, using the port
+provided by Render's environment variable.
 gunicorn --bind 0.0.0.0:$PORT app:app
