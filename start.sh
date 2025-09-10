e340d1f27820aa0b2ee90a7f64c2da3dd87d874e
moffatmubea <48717239+mubeakungu@users.noreply.github.com>
2025-09-10 11:36:28 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-Run database schema reset
 echo "Resetting database schema..."
 python db_reset.py
-
-Start the web server
 gunicorn --bind 0.0.0.0:10000 --workers 4 --threads 2 app:app
