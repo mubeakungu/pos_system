8b7f9483689105259828cb6898cf561d154822f7
moffatmubea <48717239+mubeakungu@users.noreply.github.com>
2025-09-10 11:11:00 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
-#!/usr/bin/env sh
+# start.sh
+
+# Set the FLASK_APP environment variable so Flask knows where to find the app
+export FLASK_APP=app.py
+
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
+
+# Run database migrations
 flask db upgrade
+
+# Start the Gunicorn server, binding to the port provided by Render
 gunicorn --bind 0.0.0.0:$PORT app:app
+
